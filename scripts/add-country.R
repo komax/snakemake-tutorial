74e0a5406fe799941a88b20f8675664928694cac
Maximilian Konzack <maximilian.konzack@idiv.de>
2020-02-18 15:50:21 +0100

    Output in R script

--- scripts/add-country.R
+++ scripts/add-country.R
@@ -1,10 +1,10 @@
 # Be reproducible by fixing a random seed. 
 set.seed(42)
 
+print("Adding countries...")
+
 # The countries of which we sample.
 countries <- c("Iceland", "Australia", "Germany", "US", "Japan", "Canada", "South Africa", "China")
-
-print("I am in R")
 
 # Read the csv into a dataframe.
 my_data <- read.csv(snakemake@input[[1]])
@@ -16,5 +16,5 @@ country_samples <- sample(countries, nrow(my_data), replace = TRUE)
 my_data$country <- country_samples
 
 # Output the dataframe with the country samples.
-print(snakemake@output[[1]])
+paste("Done with file =", snakemake@output[[1]])
 write.csv(my_data, file = snakemake@output[[1]])
